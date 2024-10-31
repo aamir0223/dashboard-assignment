12553860754e464d04eb724077aeafb82d3a2daf
Aamir khan <ak397783@gmail.com>
2024-10-31 15:12:06 +0530

    json data convert

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Routes as AllRoutes, Route, Navigate } from "react-router-dom";
 import Dashboard from '../Page/Dashboard';
+import CsvToJsonConverter from '../Page/CsvToJsonConverter';
 
 
 const Routes = () => {
     return (
         <AllRoutes>
-            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/Dashboard" element={<CsvToJsonConverter />} />
         </AllRoutes>
     )
 }
